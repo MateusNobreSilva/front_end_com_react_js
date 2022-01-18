de6b9fdc0506140305e086849eb8d159f5274028
MateusNobreSilva <mateusnobresilva@gmail.com>
2022-01-18 14:42:13 -0300

    Projeto Finalizado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import apifrom from './services/api';
+
+import './App.css';
+import backgroundImage from './assets/background.jpeg';
 
 import Header from './components/Header';
+import api from './services/api';
 /**
  * 
  * @returns Os 3 Conceitos Mais importantes do React
@@ -20,23 +25,40 @@ import Header from './components/Header';
 
 function App() {
 
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-End Web']);
+    const [projects, setProjects] = useState([]);
 
-    function handleAddProject() {
+    useEffect(() => {
+        api.get('projects').then(response => {
+            console.log(response);
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject() {
         //    projects.push(`Novo Projeto ${Date.now()}`);
 
-        setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+        // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
 
+        const response = await api.post('projects', {
+            title: `Novo Projeto ${Date.now()}`,
+            owner: "Astora"
+        });
 
-        console.log(projects);
+        const project = response.data;
+
+        setProjects([...projects, project]);
+
+        // console.log(projects);
     }
 
     return (
         <>
             <Header title="Mateus" />
 
+            <img width={100} src={backgroundImage} />
+
             <ul>
-                {projects.map(project => <li key={project} >{project}</li>)}
+                {projects.map(project => <li key={project.id} >{project.title}</li>)}
             </ul>
 
             <button type="button" onClick={handleAddProject}  >Adicionar Projeto</button>
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-   
+
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         path: path.resolve(__dirname, 'public'),
@@ -21,14 +21,25 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                 },
-                // query: {
-                //     presets: ['es2015']
-                // }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ],
+            },
+            {
+                test: /.*\.(gif|png|jpe?g)$/i,
+                //  test: /.*\.(gif|jpe?g|bmp|png)$/igm,
+                use: {
+                    loader: 'file-loader'
+                }
             }
         ]
     },
